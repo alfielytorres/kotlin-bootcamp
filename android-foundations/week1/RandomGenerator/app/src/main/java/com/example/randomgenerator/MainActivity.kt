1704774bfffaf89b2d0c9fca9e53d8f7f62945fe
Alfie Torres <alfielytorres@gmail.com>
2020-07-16 14:30:50 +1000

    added first project

--- android-foundations/week1/RandomGenerator/app/src/main/java/com/example/randomgenerator/MainActivity.kt
+++ android-foundations/week1/RandomGenerator/app/src/main/java/com/example/randomgenerator/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.randomgenerator
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val number: TextView =findViewById(R.id.number_textView)
+        val generateButton:Button = findViewById(R.id.generate_button)
+
+        fun rollDice(){
+            Toast.makeText(this,"Generated Number",Toast.LENGTH_SHORT).show()
+            number.text= (Random.nextInt(12)+1).toString()
+        }
+        generateButton.setOnClickListener {rollDice() }
     }
+
+
 }
+
